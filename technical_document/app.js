b3b1b6c69fd2b8743eb61a7c98e7384c3283d2d0
Mohit vishwakarma <mohitvishwakarma7891@gmail.com>
2021-01-19 17:08:55 +0530

    completed technical documentation page

---
+++ technical_document/app.js
@@ -0,0 +1,20 @@
+// document.querySelector('.navbar').addEventListener('click', function() {
+//     console.log('worked');
+//   if(document.querySelector('.navbar').style.width='250px')
+//   {
+//     document.querySelector('.navbar').style.width='50px'; 
+//     console.log('if worked');
+//   } 
+
+//   else if(document.querySelector('.navbar').style.width <='50px'){
+//     // document.querySelector('.navbar').style.width='250px'; 
+//     document.querySelector('.navbar').style.width='100px';
+//     console.log('else worked');
+//   }
+//bar---f0c9
+//cross---f00d
+// });
+
+function toggleSidebar() {
+    document.querySelector('.navbar').classList.toggle('active');
+}
